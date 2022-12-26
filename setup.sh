555b68238a1a3c3361fda5e12382c81aec0d5885
toledo <moralescl@pm.me>
2022-12-26 13:11:03 -0500

    release openai-chat-v1.0.0

---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "Installing OpenAI Chat..."
+
+#Install binary
+echo "Installing binary openai-chat into /usr/bin"
+cp openai-chat /usr/bin/
+if [ $? -ne 0 ]; then # If last command failed
+  echo "Install FAILED"
+  exit 1 # Exit with an error code
+fi
+
+#Copy desktop file
+echo "Installing OpenAI Chat desktop shortcut into /usr/share/applications"
+cp assets/openai.desktop /usr/share/applications/
+if [ $? -ne 0 ]; then # If last command failed
+  echo "Install FAILED"
+  exit 1 # Exit with an error code
+fi
+
+#Copy icon
+echo "Installing desktop icon into /usr/share/icons/hicolor/256x256/apps" 
+cp assets/openai.png /usr/share/icons/hicolor/256x256/apps/
+if [ $? -ne 0 ]; then # If last command failed
+  echo "Install FAILED"
+  exit 1 # Exit with an error code
+fi
+echo 
+echo "OpenAI Chat successfully installed! Be excited this app is amazing"
+echo
+echo "Make sure you follow the README at step 1. to create an environment variable for your OpenAI API key"
+echo
+echo "Thank you for installing OpenAI Chat"
+echo "Toledo Systems: toledosystems@pm.me"
+echo "Please consider a donation so this research may continue"
+echo "BTC address: bc1qmaz9u7eh63e4vtkv44k65la387sy6rva5s0tlp"
+echo "Paypal: toledosystems@pm.me"
+
---
+++ uninstall.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Removing OpenAI Chat from your system..."
+
+rm -f /usr/bin/openai-chat
+if [ $? -ne 0 ]; then # If last command failed
+  echo "FAILED to delete the openai-chat binary from /usr/bin/"
+  exit 1 # Exit with an error code
+fi
+
+rm -f /usr/share/applications/openai.desktop
+if [ $? -ne 0 ]; then # If last command failed
+  echo "FAILED to delete the openai-chat shortcut from /usr/share/applications/"
+  exit 1 # Exit with an error code
+fi
+
+rm -f /usr/share/icons/hicolor/256x256/apps/openai.png
+if [ $? -ne 0 ]; then # If last command failed
+  echo "FAILED to delete the openai-chat icon from /usr/share/icons/hicolor/256x256/apps"
+  exit 1 # Exit with an error code
+fi
+
+echo "OpenAI chat was successfully removed from your system, the home configuration must be deleted manually"
